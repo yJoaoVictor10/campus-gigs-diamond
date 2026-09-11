f1448d5d0733359dd17e9fb5057e6fa8f2a6462c
João Victor <jvna07@gmail.com>
2026-09-10 22:53:42 -0300

    Add files via upload

---
+++ auth/AuthController.java
@@ -0,0 +1,33 @@
+package br.com.fiap.campusgigs.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private AuthenticationManager authenticationManager;
+    private TokenService tokenService;
+
+    public record LoginRequest(String username, String password) {}
+    public record LoginResponse(String token) {}
+
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest request){
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.username(),
+                        request.password()
+                )
+        );
+
+        return new LoginResponse(tokenService.generateToken(auth.getName()));
+    }
+
+}
